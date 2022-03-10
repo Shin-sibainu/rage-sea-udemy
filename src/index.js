c9ce2ec416df4da185dffcb0598e60d79eb07ec6
Shin-sibainu <shincode0712@gmail.com>
2022-03-10 21:34:08 +0900

    almost done

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ debugObject.surfaceColor = "#66c1f9";
 const material = new THREE.ShaderMaterial({
   vertexShader: vertexShader,
   fragmentShader: fragmentShader,
+  side: THREE.DoubleSide,
   uniforms: {
     uTime: { value: 0 },
     uBigWavesElevation: { value: 0.2 },
@@ -98,6 +99,8 @@ gui
   .step(0.001)
   .name("uColorMultiplier");
 
+gui.show(false);
+
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
 //追加
@@ -123,13 +126,13 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 //カメラの起点
-camera.position.set(0, 0.22, 1);
-camera.lookAt(0, 13.5, 0);
+camera.position.set(0, 0.23, 0);
+// camera.lookAt(0, -3, 30);
 scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
 
 /**
  * Renderer
@@ -140,23 +143,66 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+//cursor
+// const cursor = {};
+// cursor.x = 0;
+// cursor.y = 0;
+// // console.log(cursor);
+// window.addEventListener("mousemove", (event) => {
+//   cursor.x = event.clientX / sizes.width - 0.5;
+//   cursor.y = event.clientY / sizes.height - 0.5;
+//   // console.log(cursor);
+// });
+
 /**
  * Animate
  */
 const clock = new THREE.Clock();
+let clickCount = 1;
+let isDive = false;
 
 const animate = () => {
   //時間取得
   const elapsedTime = clock.getElapsedTime();
+
   material.uniforms.uTime.value = elapsedTime;
 
   //カメラを円周上に周回させる。
+  camera.position.x = Math.sin(elapsedTime * 0.17) * 3.0;
+  camera.position.z = Math.cos(elapsedTime * 0.17) * 3.0;
 
-  controls.update();
+  // controls.update();
 
   renderer.render(scene, camera);
-
   window.requestAnimationFrame(animate);
+
+  if (clickCount % 2 == 0 && isDive == false) {
+    camera.position.y -= 0.01;
+
+    // console.log(camera.position.y);
+    if (camera.position.y <= -0.4) {
+      isDive = true;
+    }
+  }
+
+  if (clickCount % 2 == 1 && isDive == true) {
+    camera.position.y += 0.01;
+
+    // console.log(camera.position.y);
+    if (camera.position.y >= 0.23) {
+      isDive = false;
+    }
+  }
 };
 
 animate();
+
+/* html script */
+const diveButton = document.getElementById("diveButton");
+
+diveButton.addEventListener("click", () => {
+  // console.log("clicked");
+  //カメラを移動
+  clickCount++;
+  console.log(clickCount);
+});
